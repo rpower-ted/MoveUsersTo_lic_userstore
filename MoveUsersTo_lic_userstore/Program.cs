29c46afd016446c32e853cf56d3ab9f5b3b761d8
rpower-ted <ted.fischer@rpower.com>
2018-09-25 14:12:29 -0700

    first commit

--- MoveUsersTo_lic_userstore/Program.cs
+++ MoveUsersTo_lic_userstore/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MoveUsersTo_lic_userstore
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var userMover = new UserMover();
+            userMover.Run();
         }
     }
 }
---
+++ MoveUsersTo_lic_userstore/UserMover.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MoveUsersTo_lic_userstore
+{
+    /// <summary>
+    /// This class moves user records from tbl_user to lic_storeuser
+    /// </summary>
+    public class UserMover
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UserMover()
+        {
+
+        }
+
+        /// <summary>
+        /// Moves users from tbl_user to lic_storeuser
+        /// </summary>
+        public void Run()
+        {
+
+        }
+    }
+}
